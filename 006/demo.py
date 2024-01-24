56fb81e63fdcab386c1417a7ceec4b392110cdc8
nguyentientuanh <anhnguyentu238@gmail.com>
2024-01-24 19:17:09 +0700

    bo sung 6

---
+++ 006/demo.py
@@ -0,0 +1,17 @@
+import math
+print("toi"," ","ffyhf")
+print("toi","ffyhf")
+a ="toi"
+b="sao"
+print(a,b)
+firstname="Nguyen"
+middlename="Tien Tu"
+lastname="Anh"
+name=firstname+middlename+lastname
+print(name)
+
+A="1"
+B=1
+#C=A+B
+C=A+str(B)
+print(C)
