6b0ddf2fffcd11e485e4ee7d62b74b76e7c209aa
nguyentientuanh <anhnguyentu238@gmail.com>
2024-01-26 21:37:23 +0700

    Nop BTVN 8

--- 007/demo.py
+++ 007/demo.py
@@ -15,23 +15,30 @@
 #     x=-b/a
 #     print("nghiem cua pt la ",x)
 
-a=input("nhap so :")
-b=input("nhap so :")
-c=input("nhap so :")
+a=input("nhap so a :")
+b=input("nhap so b:")
+c=input("nhap so c:")
 
-if(a==b and a==c):
-    print("ca ba so bang nhau = {}".format(a))
-elif(a==b and a>c):
-    print("hai so lon nhat = {}".format(a))
-elif(a==c and a>b):
-    print("hai so lon nhat = {}".format(a))
-elif(c==b and c>a):
-    print("hai so lon nhat = {}".format(b))
-elif(a>b and b>c):
-    print("a lon nhat ={}".format(a))
-elif(b>a and b>c):
-    print("so lon nhat ={}".format(b))
-elif(c>a and c>b):
-    print("so lon nhat ={}".format(c))
-else:
-    print("gia tri nhaop vao khong hop le")
+# if(a==b and a==c):
+#     print("ca ba so bang nhau = {}".format(a))
+# elif(a==b and a>c):
+#     print("hai so lon nhat = {}".format(a))
+# elif(a==c and a>b):
+#     print("hai so lon nhat = {}".format(a))
+# elif(c==b and c>a):
+#     print("hai so lon nhat = {}".format(b))
+# elif(a>b and b>c):
+#     print("a lon nhat ={}".format(a))
+# elif(b>a and b>c):
+#     print("so lon nhat ={}".format(b))
+# elif(c>a and c>b):
+#     print("so lon nhat ={}".format(c))
+# else:
+#     print("gia tri nhaop vao khong hop le")
+
+max=a
+if b>max:
+    max=b
+if max<c:
+    max=c
+print(max)
